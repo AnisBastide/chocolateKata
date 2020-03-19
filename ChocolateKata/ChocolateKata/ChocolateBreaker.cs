1c5d597f6fa9a0f2df85b05da9cfa687434ee546
anisbastide <anis.bastide@upslide.net>
2020-03-19 09:21:03 +0100

    Kata done

---
+++ ChocolateKata/ChocolateKata/ChocolateBreaker.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChocolateKata
+{
+    public class ChocolateBreaker
+    {
+        private int count;
+
+        public static int BreakChocolate(int n, int m)
+        {
+            if (n == 1)
+            {
+                return m - 1;
+            }
+            if (m == 1)
+            {
+                return n - 1;
+            }
+            return 0;
+        }
+
+        public int BreakChocolateAlgo(int n, int m)
+        {
+            while ((n >= 1 && m > 1) || (n > 1 && m >= 1))
+            {
+                if (n != 1)
+                {
+                    if (n % 2 == 0)
+                    {
+                        n -= 1;
+                        count++;
+                        count += m - 1;
+                    }
+                    if (n % 2 != 0 && n != 1)
+                    {
+                        n -= 1;
+                        count++;
+                        count += m - 1;
+                    }
+                }
+
+                if (m != 1)
+                {
+                    if (m % 2 == 0)
+                    {
+                        m -= 1;
+                        count++;
+                        count += n - 1;
+                    }
+
+                    if (m % 2 != 0 && m != 1)
+                    {
+                        m -= 1;
+                        count++;
+                        count += n - 1;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ ChocolateKata/ChocolateKataTest/ChocolateBreakerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChocolateKata;
+using NUnit.Framework;
+
+namespace ChocolateKataTest
+{
+    public class ChocolateBreakerTests
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test]
+            public void TestWithColumnOrLineIsOne()
+            {
+                var chocolate = new ChocolateBreaker();
+                Assert.AreEqual(2, chocolate.BreakChocolateAlgo(1, 3));
+                chocolate = new ChocolateBreaker();
+                Assert.AreEqual(2, chocolate.BreakChocolateAlgo(3, 1));
+            }
+
+            [Test]
+            public void NoBreakNeeded()
+            {
+                var chocolate = new ChocolateBreaker();
+                Assert.AreEqual(0, chocolate.BreakChocolateAlgo(1, 1));
+            }
+
+            [Test]
+            public void FiveBreaks()
+            {
+                var chocolate = new ChocolateBreaker();
+                Assert.AreEqual(5, chocolate.BreakChocolateAlgo(3, 2));
+            }
+
+            [Test]
+            public void TwentyFourBreaks()
+            {
+                var chocolate = new ChocolateBreaker();
+                Assert.AreEqual(24, chocolate.BreakChocolateAlgo(5, 5));
+            }
+        }
+    }
+}
